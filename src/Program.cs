c12e5f67a8509a7229ba76a2453857b9542b59bc
Justin Skiles <justin.d.skiles@gmail.com>
2017-02-21 10:01:34 -0500

    Auto format. Added Win10 build runtime. Changed output assembly name to be more meaningful.

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.Extensions.CommandLineUtils;
+﻿using Microsoft.Extensions.CommandLineUtils;
 
 namespace CongressCollector
 {
@@ -9,47 +6,42 @@ namespace CongressCollector
     {
         public static void Main(string[] args)
         {
-                // Program.exe <-g|--greeting|-$ <greeting>> [name <fullname>]
-                // [-?|-h|--help] [-u|--uppercase]
-                CommandLineApplication commandLineApplication = new CommandLineApplication(throwOnUnexpectedArg: false);
-                commandLineApplication.Name = "CongressCollector";
-                commandLineApplication.FullName = "CongressCollector - For all your congressional data needs.";
+            // Program.exe <-g|--greeting|-$ <greeting>> [name <fullname>]
+            // [-?|-h|--help] [-u|--uppercase]
+            CommandLineApplication commandLineApplication = new CommandLineApplication(throwOnUnexpectedArg: false);
+            commandLineApplication.Name = "CongressCollector";
+            commandLineApplication.FullName = "CongressCollector - For all your congressional data needs.";
 
-                commandLineApplication.HelpOption("-? | -h | --help");
-                var collectionOption = commandLineApplication.Option("-b | --bulkcollection <bulkcollection>", "Bulk data collection to get such as 'billstatus'.", CommandOptionType.SingleValue);
-                var congressOption = commandLineApplication.Option("-c | --congress <congress>", "Congress to retrieve data for such as '113'.", CommandOptionType.SingleValue);
-                var billTypeOption = commandLineApplication.Option("-t | --billtype <billtype>", "Bill types to retrieve such as 'hconres'.", CommandOptionType.SingleValue);
+            commandLineApplication.HelpOption("-? | -h | --help");
+            var collectionOption = commandLineApplication.Option("-b | --bulkcollection <bulkcollection>", "Bulk data collection to get such as 'billstatus'.", CommandOptionType.SingleValue);
+            var congressOption = commandLineApplication.Option("-c | --congress <congress>", "Congress to retrieve data for such as '113'.", CommandOptionType.SingleValue);
+            var billTypeOption = commandLineApplication.Option("-t | --billtype <billtype>", "Bill types to retrieve such as 'hconres'.", CommandOptionType.SingleValue);
 
-                commandLineApplication.OnExecute(async () =>
+            commandLineApplication.OnExecute(async () =>
+            {
+                if (collectionOption.HasValue())
                 {
-                    if(collectionOption.HasValue())
+                    if (collectionOption.Value() == "billstatus")
                     {
-                        if(collectionOption.Value() == "billstatus")
+                        BulkDataProcessor bulkDataProcessor = new BulkDataProcessor(BulkDataType.BillStatus);
+
+                        if (congressOption.HasValue())
                         {
-                            BulkDataProcessor bulkDataProcessor = new BulkDataProcessor(BulkDataType.BillStatus);
+                            int congress = 0;
+                            bool success = int.TryParse(congressOption.Value(), out congress);
 
-                            if(congressOption.HasValue())
+                            if (!success)
                             {
-                                int congress = 0;
-                                bool success = int.TryParse(congressOption.Value(), out congress);
+                                return 0;
+                            }
 
-                                if(!success)
+                            if (billTypeOption.HasValue())
+                            {
+                                BillType billType = BillType.Unknown;
+                                if (billTypeOption.Value() == "hconres")
                                 {
-                                    return 0;
-                                }
-
-                                if(billTypeOption.HasValue())
-                                {
-                                    BillType billType = BillType.Unknown;
-                                    if(billTypeOption.Value() == "hconres")
-                                    {
-                                        billType = BillType.HCONRES;
-                                        await bulkDataProcessor.StartAsync(congress, billType);
-                                    }
-                                }
-                                else
-                                {
-                                    return 0;
+                                    billType = BillType.HCONRES;
+                                    await bulkDataProcessor.StartAsync(congress, billType);
                                 }
                             }
                             else
@@ -57,17 +49,22 @@ namespace CongressCollector
                                 return 0;
                             }
                         }
+                        else
+                        {
+                            return 0;
+                        }
                     }
+                }
 
-                    return 0;
-                });
-                commandLineApplication.Execute(args);
+                return 0;
+            });
+            commandLineApplication.Execute(args);
 
-                
-                // await bulkDataProcessor.StartAsync(113, BillType.HCONRES);
-                // await bulkDataProcessor.StartAsync(113, BillType.HJRES);
-                // await bulkDataProcessor.StartAsync(113, BillType.HR);
-                // await bulkDataProcessor.StartAsync(113, BillType.HRES);
+
+            // await bulkDataProcessor.StartAsync(113, BillType.HCONRES);
+            // await bulkDataProcessor.StartAsync(113, BillType.HJRES);
+            // await bulkDataProcessor.StartAsync(113, BillType.HR);
+            // await bulkDataProcessor.StartAsync(113, BillType.HRES);
         }
     }
 }
